7c676f0f3d5d0f82d9b993e8acd5d876971e224e
George Koroulis <gkoroulis7201@stu.d214.org>
2025-04-16 11:06:00 -0500

    made layout for sleep tools and inserted a few links

--- SleepTools.swift
+++ SleepTools.swift
@@ -7,6 +7,14 @@
 import SwiftUI
 
 struct SleepTools: View {
+    @State var url1 = "https://www.youtube.com/watch?v=2f5mRTjkHJ4"
+    @State var url2 = "https://www.youtube.com/watch?v=fk-_SwHhLLc"
+    @State var url3 = "https://www.mayoclinic.org/healthy-lifestyle/adult-health/in-depth/sleep/art-20048379"
+    @State var url4 = "https://www.verywellmind.com/how-to-get-better-sleep-5094084"
+    @State var url5 = "https://www.youtube.com/watch?v=zrPbSmhxUbg"
+    @State var url6 = "https://open.spotify.com/playlist/37i9dQZF1DWZ8HCIPoGGKp"
+    @State var url7 = "https://www.pandora.com/podcast/8-hour-sleep-music/green-noise-rain-drops-8-hours-of-green-noise-with-rain-sounds-under-an-umbrella-for-adhd-relief-focus-mental-clarity-and-relaxation/PE:1316770851"
+    @State var url8 = "https://music.apple.com/us/album/green-noise-nature-frequencies/1742860755"
     var body: some View {
         ZStack {
             Color(red: 0.051, green: 0.106, blue: 0.165)
@@ -20,7 +28,44 @@ struct SleepTools: View {
                             .font(.custom("American Typewriter", size: 50))
                             .foregroundStyle(Color(red: 0.918, green: 0.918, blue: 0.918))
                     }
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 50, trailing: 0))
+                Text("-- YouTube Videos --")
+                    .font(.custom("American Typewriter", size: 30))
+                    .foregroundStyle(Color(red: 0.918, green: 0.918, blue: 0.918))
+                Link(destination: URL(string: url1)!) {
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 200, height: 40)
+                        .foregroundStyle(Color(red: 0.722, green: 0.663, blue: 0.788))
+                        .overlay {
+                            Text("Guided Sleep Meditation")
+                                .foregroundStyle(Color(red: 0.918, green: 0.918, blue: 0.918))
+                        }
+                }
+                Link(destination: URL(string: url2)!) {
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 200, height: 40)
+                        .foregroundStyle(Color(red: 0.722, green: 0.663, blue: 0.788))
+                        .overlay {
+                            Text("Sleep Hygiene")
+                                .foregroundStyle(Color(red: 0.918, green: 0.918, blue: 0.918))
+                        }
+                }
+                Link(destination: URL(string: url5)!) {
+                    RoundedRectangle(cornerRadius: 10)
+                        .frame(width: 200, height: 40)
+                        .foregroundStyle(Color(red: 0.722, green: 0.663, blue: 0.788))
+                        .overlay {
+                            Text("Green Noise Playlist")
+                                .foregroundStyle(Color(red: 0.918, green: 0.918, blue: 0.918))
+                        }
+                }
+                Text("-- Articles --")
+                    .font(.custom("American Typewriter", size: 30))
+                    .foregroundStyle(Color(red: 0.918, green: 0.918, blue: 0.918))
                 
+                Text("-- Playlists --")
+                    .font(.custom("American Typewriter", size: 30))
+                    .foregroundStyle(Color(red: 0.918, green: 0.918, blue: 0.918))
             }
         }
         
