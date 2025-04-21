5c9c6e0db9b147ff07892a645585961d610d0cec
Devin Joseph <djoseph7181@stu.d214.org>
2025-04-21 10:17:16 -0500

    completed sleep science for now

--- SleepScience.swift
+++ SleepScience.swift
@@ -3,6 +3,8 @@ import SwiftUI
 struct SleepScience: View {
     let firstURL = "https://www.youtube.com/watch?v=IzQ2siryQrM"
     let secondURL = "https://www.youtube.com/watch?v=3mufsteNrTI"
+    let thirdURL = "https://pmc.ncbi.nlm.nih.gov/articles/PMC6281147/"
+    let fourthURL = "https://sleep.hms.harvard.edu/education-training/public-education/sleep-and-health-education-program/sleep-health-education-47"
     var body: some View {
         ZStack {
             Color(red: 0.051, green: 0.106, blue: 0.165)
@@ -26,7 +28,7 @@ struct SleepScience: View {
                         ZStack {
                             RoundedRectangle(cornerRadius: 5)
                                 .frame(width: 350, height: 50)
-                                .foregroundStyle(Color.blue)
+                                .foregroundStyle(Color(red: 0.722, green: 0.663, blue: 0.788))
                             Text("How Sleep Affects Your Brain")
                                 .font(.custom("American Typewriter", size: 20))
                                 .foregroundStyle(Color.white)
@@ -36,8 +38,31 @@ struct SleepScience: View {
                         ZStack {
                             RoundedRectangle(cornerRadius: 5)
                                 .frame(width: 350, height: 50)
-                                .foregroundStyle(Color.blue)
+                                .foregroundStyle(Color(red: 0.722, green: 0.663, blue: 0.788))
                             Text("Why Do We Have To Sleep?")
+                                .font(.custom("American Typewriter", size: 20))
+                                .foregroundStyle(Color.white)
+                        }
+                    })
+                    Text("--Articles--")
+                        .font(.custom("American Typewriter", size: 25))
+                        .foregroundStyle(Color.white)
+                    Link( destination: URL(string: thirdURL)!, label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .frame(width: 350, height: 50)
+                                .foregroundStyle(Color(red: 0.722, green: 0.663, blue: 0.788))
+                            Text("The Extraordinary importance of Sleep")
+                                .font(.custom("American Typewriter", size: 17))
+                                .foregroundStyle(Color.white)
+                        }
+                    })
+                    Link( destination: URL(string: fourthURL)!, label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 5)
+                                .frame(width: 350, height: 50)
+                                .foregroundStyle(Color(red: 0.722, green: 0.663, blue: 0.788))
+                            Text("Science of Sleep: What is Sleep?")
                                 .font(.custom("American Typewriter", size: 20))
                                 .foregroundStyle(Color.white)
                         }
