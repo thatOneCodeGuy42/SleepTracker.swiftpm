6e66bb9b59611b310fc73b8712bff3992e253f5d
George Koroulis <gkoroulis7201@stu.d214.org>
2025-05-05 10:33:43 -0500

    re-did the layout for the home page

--- ContentView.swift
+++ ContentView.swift
@@ -8,105 +8,114 @@
 import SwiftUI
 
 struct ContentView: View {
-        @StateObject var sleepLogViewModel = SleepLog.SleepLogViewModel()
-        
-        var body: some View {
-                NavigationStack {
-                        ZStack {
-                                Color(red: 0.051, green: 0.106, blue: 0.165)
-                                    .ignoresSafeArea()
-                                
-                                VStack {
-                                        RoundedRectangle(cornerRadius: 15)
-                                            .frame(width: 350, height: 100)
-                                            .foregroundStyle(Color(red: 0.722, green: 0.663, blue: 0.788))
-                                            .overlay {
-                                                    Text("Sleep Tracker")
-                                                        .font(.custom("American Typewriter", size: 50))
-                                                        .foregroundStyle(Color(red: 0.918, green: 0.918, blue: 0.918))
-                                                }
-                                            .padding(.bottom, 300)
-                                        
-                                        // Home Button
-                                        NavigationLink(destination: SettingsView()) {
-                                                RoundedRectangle(cornerRadius: 15)
-                                                    .frame(width: 225, height: 50)
-                                                    .foregroundStyle(Color(red: 0.722, green: 0.663, blue: 0.788))
-                                                    .overlay {
-                                                            Text("Settings")
-                                                                .font(.custom("American Typewriter", size: 30))
-                                                                .foregroundStyle(Color(red: 0.918, green: 0.918, blue: 0.918))
-                                                        }
-                                            }
-                                        .padding(.bottom, 10)
+    @StateObject var sleepLogViewModel = SleepLog.SleepLogViewModel()
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Color(red: 0.051, green: 0.106, blue: 0.165)
+                    .ignoresSafeArea()
+                
+                VStack {
                     
-                                        // Health Profile Button
-                                        NavigationLink(destination: ProfileView()) {
-                                                RoundedRectangle(cornerRadius: 15)
-                                                    .frame(width: 225, height: 50)
-                                                    .foregroundStyle(Color(red: 0.722, green: 0.663, blue: 0.788))
-                                                    .overlay {
-                                                            Text("Health Profile")
-                                                                .font(.custom("American Typewriter", size: 30))
-                                                                .foregroundStyle(Color(red: 0.918, green: 0.918, blue: 0.918))
-                                                        }
-                                            }
-                                        .padding(.bottom, 10)
+                    HStack {
+                        
+                        NavigationLink(destination: ProfileView()) {
+                            RoundedRectangle(cornerRadius: 15)
+                                .frame(width: 50, height: 50)
+                                .foregroundStyle(Color(red: 0.722, green: 0.663, blue: 0.788))
+                                .overlay {
+                                    Image(systemName: "person.circle")
+                                        .font(.custom("American Typewriter", size: 30))
+                                        .foregroundStyle(Color(red: 0.918, green: 0.918, blue: 0.918))
+                                }
+                        }
+                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 30, trailing: 0))
+                        
+                        NavigationLink(destination: SettingsView()) {
+                            RoundedRectangle(cornerRadius: 15)
+                                .frame(width: 50, height: 50)
+                                .foregroundStyle(Color(red: 0.722, green: 0.663, blue: 0.788))
+                                .overlay {
+                                    Image(systemName: "gear")
+                                        .font(.custom("American Typewriter", size: 30))
+                                        .foregroundStyle(Color(red: 0.918, green: 0.918, blue: 0.918))
+                                }
+                        }
+                        .padding(EdgeInsets(top: 0, leading: 240, bottom: 30, trailing: 0))
+                    }
                     
-                                        // Sleep Log Button
-                                        NavigationLink(destination: SleepLog(viewModel: sleepLogViewModel)) {
-                                                RoundedRectangle(cornerRadius: 15)
-                                                    .frame(width: 225, height: 50)
-                                                    .foregroundStyle(Color(red: 0.722, green: 0.663, blue: 0.788))
-                                                    .overlay {
-                                                            Text("Sleep Log")
-                                                                .font(.custom("American Typewriter", size: 30))
-                                                                .foregroundStyle(Color(red: 0.918, green: 0.918, blue: 0.918))
-                                                        }
-                                            }
-                                        .padding(.bottom, 10)
-                                        
-                                        // Sleep Trend Button
-                                        NavigationLink(destination: SleepTrendView(sleepLogViewModel: sleepLogViewModel)) {
-                                                RoundedRectangle(cornerRadius: 15)
-                                                    .frame(width: 225, height: 50)
-                                                    .foregroundStyle(Color(red: 0.722, green: 0.663, blue: 0.788))
-                                                    .overlay {
-                                                            Text("Sleep Trend")
-                                                                .font(.custom("American Typewriter", size: 30))
-                                                                .foregroundStyle(Color(red: 0.918, green: 0.918, blue: 0.918))
-                                                        }
-                                            }
-                                        .padding(.bottom, 10)
+                    RoundedRectangle(cornerRadius: 15)
+                        .frame(width: 350, height: 100)
+                        .foregroundStyle(Color(red: 0.722, green: 0.663, blue: 0.788))
+                        .overlay {
+                            Text("Sleep Tracker")
+                                .font(.custom("American Typewriter", size: 50))
+                                .foregroundStyle(Color(red: 0.918, green: 0.918, blue: 0.918))
+                        }
+                        .padding(.bottom, 300)
                     
-                                        // Sleep Science Button
-                                        NavigationLink(destination: SleepScience()) {
-                                                RoundedRectangle(cornerRadius: 15)
-                                                    .frame(width: 225, height: 50)
-                                                    .foregroundStyle(Color(red: 0.722, green: 0.663, blue: 0.788))
-                                                    .overlay {
-                                                            Text("Sleep Science")
-                                                                .font(.custom("American Typewriter", size: 30))
-                                                                .foregroundStyle(Color(red: 0.918, green: 0.918, blue: 0.918))
-                                                        }
-                                            }
-                                        .padding(.bottom, 10)
+                    // Home Button
                     
-                                        // Sleep Tools Button
-                                        NavigationLink(destination: SleepTools()) {
-                                                RoundedRectangle(cornerRadius: 15)
-                                                    .frame(width: 225, height: 50)
-                                                    .foregroundStyle(Color(red: 0.722, green: 0.663, blue: 0.788))
-                                                    .overlay {
-                                                            Text("Sleep Tools")
-                                                                .font(.custom("American Typewriter", size: 30))
-                                                                .foregroundStyle(Color(red: 0.918, green: 0.918, blue: 0.918))
-                                                        }
-                                            }
-                                    }
+                    
+                    // Health Profile Button
+                    
+                    .padding(.bottom, 10)
+                    
+                    // Sleep Log Button
+                    NavigationLink(destination: SleepLog(viewModel: sleepLogViewModel)) {
+                        RoundedRectangle(cornerRadius: 15)
+                            .frame(width: 225, height: 50)
+                            .foregroundStyle(Color(red: 0.722, green: 0.663, blue: 0.788))
+                            .overlay {
+                                Text("Sleep Log")
+                                    .font(.custom("American Typewriter", size: 30))
+                                    .foregroundStyle(Color(red: 0.918, green: 0.918, blue: 0.918))
                             }
                     }
+                    .padding(.bottom, 10)
+                    
+                    // Sleep Trend Button
+                    NavigationLink(destination: SleepTrendView(sleepLogViewModel: sleepLogViewModel)) {
+                        RoundedRectangle(cornerRadius: 15)
+                            .frame(width: 225, height: 50)
+                            .foregroundStyle(Color(red: 0.722, green: 0.663, blue: 0.788))
+                            .overlay {
+                                Text("Sleep Trend")
+                                    .font(.custom("American Typewriter", size: 30))
+                                    .foregroundStyle(Color(red: 0.918, green: 0.918, blue: 0.918))
+                            }
+                    }
+                    .padding(.bottom, 10)
+                    
+                    // Sleep Science Button
+                    NavigationLink(destination: SleepScience()) {
+                        RoundedRectangle(cornerRadius: 15)
+                            .frame(width: 225, height: 50)
+                            .foregroundStyle(Color(red: 0.722, green: 0.663, blue: 0.788))
+                            .overlay {
+                                Text("Sleep Science")
+                                    .font(.custom("American Typewriter", size: 30))
+                                    .foregroundStyle(Color(red: 0.918, green: 0.918, blue: 0.918))
+                            }
+                    }
+                    .padding(.bottom, 10)
+                    
+                    // Sleep Tools Button
+                    NavigationLink(destination: SleepTools()) {
+                        RoundedRectangle(cornerRadius: 15)
+                            .frame(width: 225, height: 50)
+                            .foregroundStyle(Color(red: 0.722, green: 0.663, blue: 0.788))
+                            .overlay {
+                                Text("Sleep Tools")
+                                    .font(.custom("American Typewriter", size: 30))
+                                    .foregroundStyle(Color(red: 0.918, green: 0.918, blue: 0.918))
+                            }
+                    }
+                }
             }
+        }
+    }
 }
 
 
