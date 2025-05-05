aae27186ed06bf198cc3325ccca56d8c722ef971
George Koroulis <gkoroulis7201@stu.d214.org>
2025-05-05 10:43:48 -0500

    started to build a screen splash

---
+++ splashScreen.swift
@@ -0,0 +1,25 @@
+//
+//  Untitled 3.swift
+//  SleepTracker
+//
+//  Created by George Koroulis on 4/25/25.
+//
+
+//import SwiftData
+
+
+import SwiftUI
+
+struct SplashScreen: View {
+    @State private var showSplashScreen = true
+    
+    var body: some View {
+        ZStack {
+            if showSplashScreen {
+                ZStack {
+                    
+                }
+            }
+        }
+    }
+}
